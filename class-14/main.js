a07e1002b3a78aa9fecd23c5d79a0f729c626386
Zakir Hossen <web.zakirbd@gmail.com>
2020-10-03 13:03:26 +0600

    class 14 code add

---
+++ class-14/main.js
@@ -0,0 +1,20 @@
+// js single line comment
+
+/*
+js multi line comment
+console.info('hello');
+*/
+
+
+// Variable
+let greeting = 'hello';
+
+let number1 = 9;
+let number2 = 9;
+
+if(){
+    console.log();
+}else {
+    console.log();
+}
+
