5a8c9811ee15ba1efc8fc2f9e04f11f0760b8a43
Zakir Hossen <web.zakirbd@gmail.com>
2020-10-03 13:03:13 +0600

    class 13 modified

--- class-13/js/main.js
+++ class-13/js/main.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 1,
+              slidesToScroll: 1,
               infinite: true,
               dots: true
             }
@@ -18,8 +18,8 @@ $(document).ready(function(){
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
