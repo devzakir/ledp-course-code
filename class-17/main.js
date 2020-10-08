c6bf0070610c77ba3051908fddb2995e7a9a1102
Zakir Hossen <web.zakirbd@gmail.com>
2020-10-08 11:05:55 +0600

    class 17 add

---
+++ class-17/main.js
@@ -0,0 +1,33 @@
+// var user = {
+//     name: "Zakir Hossen",
+//     age: 22,
+// };
+
+// user.isAdmin = true;
+// console.log(user);
+
+
+// delete user.age;
+
+// console.log(user);
+
+// let name = 'Zakir';
+
+// name = 'Zakir Hossen';
+// console.log(name);
+
+
+// var user = {
+//     "full name": 'Zakir Hossen',
+// }
+
+// console.log(user['full name']);
+
+// var now = new Date();
+
+// console.log(now.getSeconds());
+
+// var age = prompt('What is your age', '25');
+
+// console.log('Your age is '+ age);
+
